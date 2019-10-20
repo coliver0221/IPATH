120b973d65547934206ea7c0972c92620238346c
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2019-10-20 14:15:34 +0800

    make Login Page unique

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="container">
+        <div id="Oval-Copy"></div>
+        <div id="Oval"></div>
+        <div id="Rectangle"></div>
+        <div id="Oval_in"></div>
+        <video id="scanner"></video>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
