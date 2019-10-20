68a3834321f16c006015896bd50492ad3f7f1d0c
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2019-10-20 21:00:50 +0800

    modify Login.js

--- src/Login.js
+++ src/Login.js
@@ -3,16 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div id="container">
-        <div id="Oval-Copy"></div>
-        <div id="Oval"></div>
-        <div id="Rectangle"></div>
-        <div id="Oval_in"></div>
-        <video id="scanner"></video>
-
-      </div>
-      
+    <div id="login">
+      <div id="Oval-Copy"></div>
+      <div id="Oval"></div>
+      <div id="Rectangle"></div>
+      <div id="Oval_in"></div>
+      <video id="scanner"></video>
     </div>
   );
 }
