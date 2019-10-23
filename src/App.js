64438812591cff7238890703c6383a7d6c9ea57b
TseHang <a54383813@gmail.com>
2019-10-23 16:21:26 +0800

    fix route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Login from './Login';
-import Profile from './Profile';
-import Shop from './Shop';
-import Event from './Event';
-import ResetStyles from './theme/ResetStyles';
-import GlobalStyles from './theme/GlobalStyles';
+import Login from "./Login";
+import Profile from "./Profile";
+import Event from "./Event";
+import Shop from "./Shop";
+import ResetStyles from "./theme/ResetStyles";
+import GlobalStyles from "./theme/GlobalStyles";
+
+// for github env
+const baseName = process.env.NODE_ENV === "production" ? "/IPATH" : "";
 
 function App() {
   return (
-    <Router>
+    <Router basename={baseName}>
       <div className="App">
         <Switch>
-          <Route path='/' exact component={Login} />
-          <Route path='/profile' component={Profile} />
-          <Route path='/shop' component={Shop} />
-          <Route path='/event' component={Event} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/event" component={Event} />
+          <Route path="/" exact component={Login} />
         </Switch>
       </div>
       <ResetStyles />
