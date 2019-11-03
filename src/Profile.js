aeb30e94652f8546a6a6343cd5206f791a346f76
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2019-11-03 16:56:41 +0800

    refactor(profile.js): refactoring with styled and add background img

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,83 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 import userImg from './img/principle.png';
+import bodyImg from './img/profilebb.png';
+
+const InnerBoby = styled.div`
+    height: 46.5vh;
+    background-color: #e8e8e8;
+    text-align: center;
+    background-image: url(${bodyImg});
+    background-repeat: no-repeat;
+    background-position: bottom;
+
+    .topBody {
+        width: 100%;
+        height: 15vw;
+        background-color: #ffffff;
+    }
+`;
+
+const UserImg = styled.img`
+    width: 31.5vw;
+    height: 31.5vw;
+    border-radius: 50%;
+    border: solid 10px #ffffff;
+    z-index: 5;
+    transform: translateY(-8vh);
+`;
+
+const UserInfo = styled.div`
+    width: 100%;
+    line-height: 1.5;
+    transform: translateY(-2vh);
+
+    .nickName {
+        margin-top: 2vh;
+    }
+`;
+
+const BottomBody = styled.div`
+    width: 100%;
+    height: 20vh;
+    background-color: #ffffff;
+    text-align: center;
+    position: absolute;
+    bottom: 10.5vh;
+    display: flex;
+    justify-content: center;
+`;
+
+const UserPoint = styled.div`
+    display: flex;
+    flex-position: row;
+    justify-content: center;
+    align-items: center;
+
+    .dot {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: #d04b00;
+    }
+
+    p {
+        font-size: 26px;
+        color: #4a4a4a;
+        display: inline-block;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .point {
+        color: #d04b00;
+    }
+`;
 
 function Profile() {
     const testing_point = 116;
@@ -12,23 +85,23 @@ function Profile() {
     return (
         <div className="profile">
             <Nav title={"IPATH 學生點數系統"}/>
-            <div className="innerBoby">
-                <div className="topBody"></div>
-                <img src={userImg} alt="userImg" className="userImg" />
-                <div className="userInfo">
+            <InnerBoby>
+                <div className="topBody" />
+                <UserImg src={userImg}/>
+                <UserInfo>
                     <p className="userName">UserName</p>
                     <p className="nickName">NickName</p>
-                </div>
-            </div>
-            <div className="bottomBody">
-                <div className="userPoint">
-                    <div className="dot"></div>
+                </UserInfo>
+            </InnerBoby>
+            <BottomBody>
+                <UserPoint>
+                    <div className="dot" />
                     <p>目前持有 </p>
                     <p className="point">{testing_point}</p>
                     <p> 點</p>
-                    <div className="dot"></div>
-                </div>
-            </div>
+                    <div className="dot" />
+                </UserPoint>
+            </BottomBody>
             <Footer currPage={"profile"} />
         </div>
     );
